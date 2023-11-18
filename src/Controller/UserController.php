7d1daf655c636608da91137a2df5a6922c0985fb
kiseolar-ruslan <kretsu.rus@gmail.com>
2023-11-18 22:29:08 +0200

    add: 2 templates for viewing user data

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -12,66 +12,70 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/users')]
 class UserController extends AbstractController
 {
-    #[Route('/', name: 'app_user')]
-    public function index(): Response
+    #[Route('/', name: 'show_users')]
+    public function showAllUsers(EntityManagerInterface $em): Response
     {
+        $repo  = $em->getRepository(User::class);
+        $users = $repo->findAll();
+
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
+            'users' => $users,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'user_info', requirements: ['id' => '\d+'], methods: 'GET')]
+    public function getData(User $user): Response
+    {
+        return $this->render('user/user_info.html.twig', [
+            'controller_name' => 'UserController',
+            'user'            => $user,
         ]);
     }
 
 
+    #[Route('/generate', name: 'generate_users')]
+    public function generate(EntityManagerInterface $em): Response
+    {
+        $users = [
+            [
+                'login'  => 'Maks',
+                'pass'   => '123123',
+                'status' => User::STATUS_ACTIVE,
+                'phones' => [
+                    '+380685508373',
+                    '+380685508322',
+                ]
+            ],
+            [
+                'login'  => 'Devoid',
+                'pass'   => '123321',
+                'status' => User::STATUS_VIP,
+                'phones' => [
+                    '+380685508383',
+                ]
+            ],
+            [
+                'login'  => 'Nick',
+                'pass'   => '321321',
+                'status' => User::STATUS_DISABLED,
+                'phones' => [
+                    '+380685508133',
+                ]
+            ],
+        ];
 
-
-
-
-
-
-
-
-
-//    #[Route('/generate', name: 'generate_users')]
-//    public function generate(EntityManagerInterface $em): Response
-//    {
-//        $users = [
-//            [
-//                'login' => 'Maks',
-//                'pass' => '123123',
-//                'status' => User::STATUS_ACTIVE,
-//                'phones' => [
-//                    '+380685508373',
-//                    '+380685508322',
-//                ]
-//            ],
-//            [
-//                'login' => 'Devoid',
-//                'pass' => '123321',
-//                'status' => User::STATUS_VIP,
-//                'phones' => [
-//                    '+380685508383',
-//                ]
-//            ],
-//            [
-//                'login' => 'Nick',
-//                'pass' => '321321',
-//                'status' => User::STATUS_DISABLED,
-//                'phones' => [
-//                    '+380685508133',
-//                ]
-//            ],
-//        ];
+//    foreach ($users as $user) {
+//        $userObj = new User($user['login'], $user['pass'], $user['status']);
+//        $em->persist($userObj);
 //
-//        foreach ($users as $user) {
-//            $userObj = new User($user['login'], $user['pass'], $user['status']);
-//            $em->persist($userObj);
-//
-//            foreach ($user['phones'] as $phone) {
-//                $phoneObj = new Phone($userObj, $phone);
-//                $em->persist($phoneObj);
-//            }
+//        foreach ($user['phones'] as $phone) {
+//            $phoneObj = new Phone($userObj, $phone);
+//            $em->persist($phoneObj);
 //        }
-//        $em->flush();
-//
-//        return new Response('OK');
 //    }
+//        $em->flush();
+
+        return new Response('OK');
+    }
 }
--- test.php
+++ test.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 require_once __DIR__ . '/vendor/autoload.php';
 
 $a = [
-    'first' => [
+    [
         'a' => 1,
         'b' => 2,
         'c' => 3,
@@ -16,7 +16,12 @@ $a = [
     ],
 ];
 
-foreach ($a as $item) {
-    echo $item['a'];
+//foreach ($a as $item) {
+//    echo $item['c'];
+//}
+
+$test = [1, 2, 3, 4, 5];
+
+for ($i = 0; $i < count($test); $i++) {
+    echo $test[$i] . PHP_EOL;
 }
-exit;
